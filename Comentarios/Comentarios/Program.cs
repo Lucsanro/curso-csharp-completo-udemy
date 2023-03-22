012629decd869a26e0cabc1c39ecf4c6ee5547c1
Lucca Rocha <lucca.rocha@outlook.com>
2023-03-22 12:36:03 -0300

    Seção 3
    Organizar o código com comentários

---
+++ Comentarios/Comentarios/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comentarios
+{
+    internal class Program
+    {
+        // Método principal do projeto
+        // Mais uma linha para comentario
+        static void Main(string[] args)
+        {
+
+            #region Bloco Comentario
+            /*
+             Este é um bloco de comentario
+             Posso escrever varias linhas
+             Que serão consideradas comentários
+            */
+            #endregion
+
+            #region Primeira Parte
+            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            #endregion
+
+            #region Segunda Parte
+            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            #endregion
+
+            #region Terceira Parte
+            Console.ReadKey();
+            #endregion
+
+
+        }
+    }
+}
