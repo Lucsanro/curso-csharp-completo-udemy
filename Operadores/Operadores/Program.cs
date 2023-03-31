97d6345ca32802a672cb17f9c8c7037dfc664f0d
Lucca Rocha <lucca.rocha@outlook.com>
2023-03-30 23:28:24 -0300

    Seção 7
    Operadores aritméticos, precedência, incremento e decremento, concatenação, atribuição, igualdade, relacionais e lógicos

---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Operadores matemáticos
+            //int num1 = 15;
+            //int num2 = 2;
+
+            ////int resultado = num1 + num2;
+
+            //Console.WriteLine(num1 + num2 + " Adição de " + num1 + " + " + num2);
+            //Console.WriteLine(num1 - num2 + " Subtração de " + num1 + " - " + num2);
+            //Console.WriteLine(num1 * num2 + " Multiplicação de " + num1 + " * " + num2);
+            //Console.WriteLine(num1 / num2 + " Divisão de " + num1 + " / " + num2);
+            //Console.WriteLine(num1 % num2 + " Resto de divisão de " + num1 + " % " + num2);
+
+            //Console.ReadKey();
+            #endregion
+
+            #region Precedência de Operadores
+            //double num1 = 100;
+            //double num2 = 10;
+            //double num3 = 5;
+
+            //double res = (num1 + num2) * (num3 / 3);
+            #endregion
+
+            #region Incremento e decremento
+            //int num1 = 10;
+
+            ////num1 = num1 + 1;
+            ////num1++;
+            ////++num1;
+            ////num1--;
+            ////--num1;
+            #endregion
+
+            #region Concatenação
+            //string nome = "Gabriel ";
+            //string sobreNome = "Artigas";
+            //string nomeCompleto = nome + sobreNome + " " + 1985;
+            #endregion
+
+            #region Operadores Atribuição
+            //int num1 = 13;
+
+            //num1 = num1 + 20;
+            //num1 += 20;
+            //num1 -= 2;
+            //num1 *= 3;
+            //num1 /= 2;
+            //num1 %= 2;
+
+            //string nome = "Gabriel";
+            //nome += " Artigas";
+            #endregion
+
+            #region Igualdade / Comparação
+            //bool res = 100 == (50 / 2);
+            //bool res2 = (200 / 2) != (100 + 100);
+
+            //string nome = "Gabriel";
+            //bool res3 = nome != "Lucas";
+            #endregion
+
+            #region Operadores Relacionais
+            /*
+            Maior que >
+            Menor que <
+            Maior ou igual que >=
+            Menor ou igual que <=
+            */
+
+            //bool res1 = 100 < 50 * 2;
+            //bool res2 = 100 + 25 > 50 * 2;
+            //bool res3 = 100 <= 50 * 2;
+            //bool res4 = 75 >= 50;
+            #endregion
+
+            #region Operadores Lógicos
+            //bool res1 = 100 == 50;
+            //bool res2 = 60 == 50;
+
+            //bool final = res1 && res2;
+            //bool final2 = (100 == 50) || (60 == 50);
+            #endregion
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
