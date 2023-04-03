9c9d6b27f7c058a142ea82ef471b83c245999b55
Lucca Rocha <lucca.rocha@outlook.com>
2023-04-02 22:28:37 -0300

    Seção 8
    Coleções do tipo array

---
+++ Colecao/Colecao/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Arrays unidimensionais - Vetores
+            /*
+                Aula sobre vetores unidimensionais (Arrays unidimensionais) 
+            */
+
+            //int[] numeros = new int[5];
+
+            //numeros[0] = 10;
+            //numeros[1] = 20;
+            //numeros[2] = 30;
+            //numeros[3] = 40;
+            //numeros[4] = 50;
+
+            //string[] nomes = {
+            //    "Gabriel",
+            //    "Danny",
+            //    "Arthur"
+            //};
+
+            //Console.WriteLine(numeros[3]);
+            //Console.WriteLine(nomes[1]);
+            //Console.ReadKey();
+            #endregion
+
+            #region Arrays bidimensionais - Matrizes
+
+            /*
+                Aula sobre matrizes 
+            */
+
+            //int[,] numeros = new int[2, 3];
+
+            //numeros[0, 0] = 10; 
+            //numeros[0, 1] = 20;
+            //numeros[0, 2] = 30;
+
+            //numeros[1, 0] = 40;
+            //numeros[1, 1] = 50;
+            //numeros[1, 2] = 60;
+
+            //Console.Write("[" + numeros[0, 0] + "]");
+            //Console.Write("[" + numeros[0, 1] + "]");
+            //Console.Write("[" + numeros[0, 2] + "]");
+
+            //Console.WriteLine();
+
+            //Console.Write("[" + numeros[1, 0] + "]");
+            //Console.Write("[" + numeros[1, 1] + "]");
+            //Console.WriteLine("[" + numeros[1, 2] + "]");
+
+            //string[,] nomes = {
+            //    {"Gabriel", "Arthur", "Danny"},
+            //    {"Flávio", "Glória", "Bia" }
+            //};
+
+            //Console.WriteLine(nomes[1, 1]);
+
+            //Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
