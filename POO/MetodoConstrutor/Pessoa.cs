1ca07187ee212b9c3c328f2c3fbb94c8d6a5eb39
Lucca Rocha <lucca.rocha@outlook.com>
2023-06-28 17:25:53 -0300

    Seção 12
    Programação Orientada a Objeto (POO)

---
+++ POO/MetodoConstrutor/Pessoa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoConstrutor
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public string sobrenome;
+        public int anoNascimento;
+        public int idade;
+
+        // acionado na instanciação da classe
+        public Pessoa()
+        {
+            nome = "Desconhecido";
+            sobrenome = "Nenhum";
+            anoNascimento = 0;
+            idade = 0;
+        }
+
+        private int Idade()
+        {
+            return 2023 - anoNascimento;
+        }
+
+        // overloading de método construtor
+
+        public Pessoa(string nome, string sobrenome, int anoNascimento)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.anoNascimento = anoNascimento;
+            idade = Idade();
+        }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            anoNascimento = 2002;
+            idade = Idade();
+        }
+        
+    }
+}
---
+++ POO/MetodoConstrutor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoConstrutor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pessoa p1 = new Pessoa(); // aqui o método construtor é acionado
+
+            Console.WriteLine(p1.nome);
+            Console.WriteLine(p1.sobrenome);
+            Console.WriteLine(p1.anoNascimento);
+            Console.WriteLine(p1.idade);
+
+            Pessoa p2 = new Pessoa("Gabriel", "Artigas", 1985);
+
+            Console.WriteLine();
+
+            Console.WriteLine(p2.nome);
+            Console.WriteLine(p2.sobrenome);
+            Console.WriteLine(p2.anoNascimento);
+            Console.WriteLine(p2.idade);
+
+            Pessoa p3 = new Pessoa("Arthur", "Maciel");
+
+            Console.WriteLine();
+
+            Console.WriteLine(p3.nome);
+            Console.WriteLine(p3.sobrenome);
+            Console.WriteLine(p3.anoNascimento);
+            Console.WriteLine(p3.idade);
+
+            Console.ReadKey();
+        }
+    }
+}
